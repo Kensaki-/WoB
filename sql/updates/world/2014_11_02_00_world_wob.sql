1b42a3a4450554165d0cdd240034d710e55b7885
kensaki- <jakbojop@msn.com>
2014-11-02 19:04:27 +0100

    too low speed caused server errors.

--- sql/updates/world/2014_11_02_00_world_wob.sql
+++ sql/updates/world/2014_11_02_00_world_wob.sql
@@ -1,2 +1,2 @@
 -- Stop Nerub'ar Victim eggs from moving, and remove experience gain
-UPDATE `creature_template` SET `speed_walk`=0.0001, `speed_run`=0.0001, `ExperienceModifier`=0 WHERE `entry`=25284
+UPDATE `creature_template` SET `speed_walk`=0.1, `speed_run`=0.1, `ExperienceModifier`=0 WHERE `entry`=25284
