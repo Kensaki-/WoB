68cbf265c16b6c4d53e5fe6f9bd155b14c2d2c64
kensaki- <jakbojop@msn.com>
2014-11-02 18:54:05 +0100

    DB/Creature: Stopped Nerub'ar Victim eggs from moving, and removed experience gain

--- sql/updates/world/2014_11_01_00_world_wob.sql
+++ sql/updates/world/2014_11_01_00_world_wob.sql
@@ -1,2 +1,2 @@
--- Made Deviate Ravager use Thrash in combat to avoid suicide
+-- Made Deviate Ravager use Thrash IN combat to avoid suicide
 UPDATE `smart_scripts` SET `event_type`=0, `comment`='Deviate Ravager - In Combat - Cast \'Thrash\'' WHERE `entryorguid`=3636
