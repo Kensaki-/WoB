7213467bf25bf7b34ace114f010051a7eef18150
kensaki- <jakbojop@msn.com>
2014-11-05 03:23:30 +0100

    DB/Creature: Nerub'ar Victims properly fixed.

--- sql/updates/world/2014_11_02_00_world_wob.sql
+++ sql/updates/world/2014_11_02_00_world_wob.sql
@@ -1,2 +1,2 @@
 -- Stop Nerub'ar Victim eggs from moving, and remove experience gain
-UPDATE `creature_template` SET `speed_walk`=0.1, `speed_run`=0.1, `ExperienceModifier`=0 WHERE `entry`=25284
+UPDATE `creature_template` SET `unit_flags`=516, `flags_extra`=64 WHERE `entry`=25284
